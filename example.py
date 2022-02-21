14f2d341516727770a402a7d7406ef8e9dd66596
ArjunSahlot <iarjun.sahlot@gmail.com>
2022-02-21 11:02:59 -0800

    add example

---
+++ example.py
@@ -0,0 +1,36 @@
+import pygame
+import vidmaker
+
+FPS = 60
+
+# Window Management
+WINDOW = pygame.display.set_mode((300, 300))
+video = vidmaker.Video(".", FPS)
+pygame.display.set_caption("vidmaker test")
+
+
+def main(window):
+    pygame.init()
+    clock = pygame.time.Clock()
+
+    while True:
+        clock.tick(FPS)
+        window.fill((255, 0, 0))
+        events = pygame.event.get()
+        keys = pygame.key.get_pressed()
+        ctrl_pressed = keys[pygame.K_LCTRL] or keys[pygame.K_RCTRL]
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q and ctrl_pressed:
+                    pygame.quit()
+                    return
+
+        video.update(pygame.surfarray.pixels3d(window))
+        pygame.display.update()
+
+
+main(WINDOW)
+video.export()
